6c63f015066c1d86f6e61eedf9b69dd95076bc38
GemmaSans <gemmasanscoll@gmail.com>
2026-08-20 13:22:47 +0200

    [added] ex3

---
+++ ex3/ft_vault_security.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+def secure_archive(filename: str,
+                   action: str = "r",
+                   content: str = "") -> tuple[bool, str]:
+    try:
+        with open(filename, action) as file:
+            if action == "w":
+                file.write(content)
+                return (True, "Content successfully written to file")
+            else:
+                text = file.read()
+                return (True, text)
+    except (FileNotFoundError, PermissionError) as error:
+        return (False, str(error))
+
+
+def main() -> None:
+    print("=== Cyber Archives Security ===\n")
+
+    print("Using 'secure_archive' to read from a nonexistent file:")
+    print(secure_archive("/not/existing/file"))
+    print()
+
+    print("Using 'secure_archive' to read from an inaccessible file:")
+    print(secure_archive("/etc/master.passwd"))
+    print()
+
+    print("Using 'secure_archive' to read from a regular file:")
+    print(secure_archive("ancient_fragment.txt"))
+    print()
+
+    print("Using 'secure_archive' to write previous content to a new file:")
+    with open("ancient_fragment.txt", "r") as file:
+        content = file.read()
+    print(secure_archive("new_file.txt", "w", content))
+
+
+if __name__ == "__main__":
+    main()
